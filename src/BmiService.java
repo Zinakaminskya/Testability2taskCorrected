850a7483047b28c77ea5248101d0668e9dc209af
Zina neto <orbit187@yandex.ru>
2025-02-21 02:13:35 +0300

    src final

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double weightInKilo, double heightInMeters) {
+    public int calculate(double weightInKilo, double heightInMeters) {
         double bmiIndex = (weightInKilo / (heightInMeters * heightInMeters));
         return (int) bmiIndex;
     }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double BMI = service.calculate(98, 1.87);
+        int BMI = service.calculate(75, 1.87);
         System.out.println(BMI);
     }
 }
